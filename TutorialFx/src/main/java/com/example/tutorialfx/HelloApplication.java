b79dcb7332d81abfa39edbec7b9dc2b0b2f51920
Emir Akar <emirakar@gmail.com>
2025-05-08 19:49:01 +0300

    Yeni JavaFX file

---
+++ TutorialFx/src/main/java/com/example/tutorialfx/HelloApplication.java
@@ -0,0 +1,44 @@
+package com.example.tutorialfx;
+
+import eu.hansolo.tilesfx.Tile;
+import javafx.animation.FadeTransition;
+import javafx.application.Application;
+import javafx.css.SizeUnits;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.effect.Bloom;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Reflection;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("SignUp.fxml"));
+        stage.setTitle("Calculator");
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
+}
