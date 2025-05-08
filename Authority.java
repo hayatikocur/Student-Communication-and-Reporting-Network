93b2267005e1ea70a99dfdc4c4bc661eee99385e
Burhan-B2240 <bulut.burhan9606@gmail.com>
2025-05-08 22:04:48 +0300

    added constructor

--- Authority.java
+++ Authority.java
@@ -1,4 +1,9 @@
 public class Authority extends User {
+
+    public Authority(int userId, String userName, String userSurname, String email, String password){
+        super(userId, userName, userSurname, email, password);
+    }
+
     public void resolveReport(ProblemReport pr) {
         pr.setResolved(true);
     }
