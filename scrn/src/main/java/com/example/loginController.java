95ed850ac0b3f875f942412c3c2a0bfb428536ea
Emir Akar <emirakar48@gmail.com>
2025-05-10 13:31:46 +0300

    Controller class for login pages is added.

---
+++ scrn/src/main/java/com/example/loginController.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class loginController {
+
+    @FXML
+    Button fPageSignUp;
+    @FXML
+    Button fPageSignIn;
+
+    public static void changeToSignUp(){
+
+    }
+
+    public static void changeToSignIn(){
+        
+    }
+    
+}
