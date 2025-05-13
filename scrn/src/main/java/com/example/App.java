5cc92f40ca3b93c8fae065e9521984fe139a9c4c
Emir Akar <emirakar48@gmail.com>
2025-05-13 19:28:19 +0300

    Login Page UI changed.

--- scrn/src/main/java/com/example/App.java
+++ scrn/src/main/java/com/example/App.java
@@ -242,7 +242,6 @@ public class App extends Application {
         return postCategories.getOrDefault(post, List.of());
     }
 
-    
     public static void main(String[] args) {
         launch();
     }
