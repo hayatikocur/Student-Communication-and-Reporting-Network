0d207d82488d03d1f934cc5e335d050e570deb32
Emir Akar <emirakar48@gmail.com>
2025-05-13 20:06:38 +0300

    Some changes to map html files.

--- scrn/src/main/java/com/example/App.java
+++ scrn/src/main/java/com/example/App.java
@@ -242,6 +242,7 @@ public class App extends Application {
         return postCategories.getOrDefault(post, List.of());
     }
 
+    
     public static void main(String[] args) {
         launch();
     }
