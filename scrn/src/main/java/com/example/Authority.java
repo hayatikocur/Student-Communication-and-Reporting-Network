b9236f9d077615cb099eaf5be868470b556118d2
Burhan <bulut.burhan9606@gmail.com>
2025-05-13 19:09:41 +0300

    errors are fixed

--- scrn/src/main/java/com/example/Authority.java
+++ scrn/src/main/java/com/example/Authority.java
@@ -10,8 +10,6 @@ public class Authority extends User {
 
     public Authority(String userName, String userSurname, String email, String password){
         super(userName, userSurname, email, password);
-
-        saveToDatabase();
     }
 
     public void resolveReport(ProblemReport pr) {
@@ -22,36 +20,6 @@ public class Authority extends User {
         // Remove from DB or list
     }
 
-    private void saveToDatabase() {
-        try {
-            Properties props = new Properties();
-            try (InputStream input = getClass().getClassLoader().getResourceAsStream("db.properties")) {
-                props.load(input);
-            }
-
-            String url = props.getProperty("db.url");
-            String dbUser = props.getProperty("db.user");
-            String dbPassword = props.getProperty("db.password");
-
-            try (Connection conn = DriverManager.getConnection(url, dbUser, dbPassword)) {
-                String sql = "INSERT INTO users (userName, userSurname, email, password, mailNotification, appNotification, isAuthority) " +
-                        "VALUES (?, ?, ?, ?, ?, ?, ?)";
-                try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-                    stmt.setString(1, userName);
-                    stmt.setString(2, userSurname);
-                    stmt.setString(3, email);
-                    stmt.setString(4, password); // You should hash this in real use
-                    stmt.setBoolean(5, true);
-                    stmt.setBoolean(6, true);
-                    stmt.setBoolean(7, true);
-
-                    stmt.executeUpdate();
-                    System.out.println("User saved successfully.");
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println("Error saving user to database.");
-        }
-    }
+    
+    
 }
